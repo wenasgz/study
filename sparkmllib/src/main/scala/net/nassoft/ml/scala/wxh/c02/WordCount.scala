3730dd631ecd2c3cd5f34df3b835e5321f1be75f
wenasgz <19480345@qq.com>
2019-01-26 14:36:57 +0800

    sparkmllib init

--- sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c02/WordCount.scala
+++ sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c02/WordCount.scala
@@ -4,9 +4,15 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local").setAppName("wordCount")	//创建环境变量
-    val sc = new SparkContext(conf)								//创建环境变量实例
-    val data = sc.textFile("c://wc.txt")								//读取文件
-    data.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)	//word计数
+    // 创建环境变量
+    val conf = new SparkConf().setMaster("local").setAppName("wordCount")
+    // 创建环境变量实例
+    val sc = new SparkContext(conf)
+    // 获取当前运行路径
+    val userPath = System.getProperty("user.dir")
+    //读取文件
+    val data = sc.textFile(userPath + "/sparkmllib/src/main/resources/data/wxh/c02/wc.txt")
+    //word计数
+    data.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).collect().foreach(println)
   }
 }
