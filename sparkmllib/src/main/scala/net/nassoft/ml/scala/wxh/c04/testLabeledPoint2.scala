f0d8a1aea694bdfa7964d0f3bf946d399fdc489b
wenasgz <19480345@qq.com>
2019-02-21 13:41:10 +0800

    sparkmllib init

--- sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testLabeledPoint2.scala
+++ sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testLabeledPoint2.scala
@@ -5,11 +5,11 @@ import org.apache.spark.mllib.util.MLUtils
 
 object testLabeledPoint2 {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local").setAppName("SparkPi")
+    val conf = new SparkConf().setMaster("local").setAppName("testLabeledPoint2")
     //建立本地环境变量
     val sc = new SparkContext(conf) //建立Spark处理
 
-    val mu = MLUtils.loadLibSVMFile(sc, "c://a.txt") //从C路径盘读取文件
+    val mu = MLUtils.loadLibSVMFile(sc, "/data/f/Workshop/study/sparkmllib/src/main/resources/data/wxh/c04/a.txt") //从路径读取文件
     mu.foreach(println) //打印内容
   }
 }
