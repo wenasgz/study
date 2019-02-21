0c3b6c355e0ad93e7f3102646fe61079fde37cb3
wenasgz <19480345@qq.com>
2019-02-21 16:12:55 +0800

    sparkmllib init

--- sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testLabeledPoint2.scala
+++ sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testLabeledPoint2.scala
@@ -8,8 +8,10 @@ object testLabeledPoint2 {
     val conf = new SparkConf().setMaster("local").setAppName("testLabeledPoint2")
     //建立本地环境变量
     val sc = new SparkContext(conf) //建立Spark处理
-
-    val mu = MLUtils.loadLibSVMFile(sc, "/data/f/Workshop/study/sparkmllib/src/main/resources/data/wxh/c04/a.txt") //从路径读取文件
+    // 获取当前运行路径
+    val userPath = System.getProperty("user.dir")
+    //读取文件
+    val mu = MLUtils.loadLibSVMFile(sc, userPath+ "/sparkmllib/src/main/resources/data/wxh/c04/testLabeledPoint2.txt") //从路径读取文件
     mu.foreach(println) //打印内容
   }
 }
--- sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testMatrix.scala
+++ sparkmllib/src/main/scala/net/nassoft/ml/scala/wxh/c04/testMatrix.scala
@@ -4,6 +4,11 @@ import org.apache.spark.mllib.linalg.Matrices
 
 object testMatrix {
   def main(args: Array[String]) {
+    /**
+      *   第一个参数是新矩阵的行数
+      *   第二个参数是新矩阵的列数
+      *   第三个参数为传入的数据值
+      */
     val mx = Matrices.dense(2, 3, Array(1, 2, 3, 4, 5, 6)) //创建一个分布式矩阵
     println(mx) //打印结果
   }
